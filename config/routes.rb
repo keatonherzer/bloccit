d578f0eeeb71851a8f0e90653640575624ae0ab3
Keaton Herzer <keatonh12@gmail.com>
2014-04-09 17:18:47 -0700

    Associated and nested topics and posts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Bloccit::Application.routes.draw do
 
   devise_for :users
+
+  resources :topics do
+    resources :posts, except: [:index]
+  end
+  
   resources :posts
 
   get 'about' => 'welcome#about'
   get 'posts' => 'posts#index'
+  get 'topics' => 'topics#index'
 
   root to: 'welcome#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,45 +1,70 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'faker'
-
+ 
+#create 15 topics
+topics = [ ] 
+15.times do
+  topics << Topic.create(
+    name: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph)
+end
+ 
+admin = User.new(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  password_confirmation: 'helloworld')
+admin.skip_confirmation!
+admin.save
+admin.update_attribute(:role, 'admin')
+ 
+moderator = User.new(
+  name: 'Moderator User',
+  email: 'moderator@example.com',
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+moderator.skip_confirmation!
+moderator.save
+moderator.update_attribute(:role, 'moderator')
+ 
+member = User.new(
+  name: 'Member User',
+  email: 'member@example.com',
+  password: 'helloworld',
+  password_confirmation: 'helloworld')
+member.skip_confirmation!
+member.save
+ 
+########################################################
+ 
 5.times do
   password = Faker::Lorem.characters(10)
   user = User.new(
-    name: Faker::Name.name,
-    email: Faker::Internet.email,
+    name: Faker::Name.name, 
+    email: Faker::Internet.email, 
     password: password, 
     password_confirmation: password)
   user.skip_confirmation!
   user.save
-
-  # Note: by calling `User.new` instead of `create`,
-  # we create an instance of a user which isn't saved to the database.
-  # The `skip_confirmation!` method sets the confirmation date
-  # to avoid sending an email. The `save` method updates the database
-
-  5.times do
+  
+  5.times do 
+    topic = topics.first
     post = Post.create(
-      user: user,
+      user: user, 
+      topic: topic,
       title: Faker::Lorem.sentence,
       body: Faker::Lorem.paragraph)
-    post.update_attribute(:created_at, Time.now - rand(600..3536000))
+    post.update_attribute(:created_at, Time.now - rand(600..31536000))
+  
+    topics.rotate!
   end
 end
-
-user = User.first
-user.skip_reconfirmation!
-user.update_attributes(email: 'keatonh12@gmail.com', password: 'helloworld', password_confirmation: 'helloworld')
-
+ 
+############################################
+ 
 puts "Seed finished"
 puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
-
 
 
 
