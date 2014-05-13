b8d3ebcf4bbe681197d37246d2b2fc2cf224e182
Keaton Herzer <keatonh12@gmail.com>
2014-05-13 15:19:57 -0700

    created user profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,11 +7,18 @@ module ApplicationHelper
     end
   end
   
-def markdown(text)
-  renderer = Redcarpet::Render::HTML.new
-  extensions = {fenced_code_blocks: true}
-  redcarpet = Redcarpet::Markdown.new(renderer, extensions)
-  (redcarpet.render text).html_safe
-end
+  def markdown(text)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render text).html_safe
+  end
+
+  def comment_url_helper(comment)
+    post = comment.post
+    topic = post.topic
+    [topic, post, comment]
+  end
+
 
 end
